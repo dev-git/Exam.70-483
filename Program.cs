6b83c387037358c5300dd9b5810cc9762d213e94
Paul Bates <bates.pauls@gmail.com>
2019-09-06 14:05:33 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam._70_483.Csl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
