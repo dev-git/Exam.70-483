1f423440fd29928d19ea168d65f3fa6d2dd78855
Paul Bates <bates.pauls@gmail.com>
2019-09-08 17:36:30 +1200

    Adding new class

---
+++ Chapter 1/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam.Seventy_483.Csl.Chapter1
+{
+    public class Task
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("Work starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work finished");
+        }
+
+        public static void CreateTask()
+        {
+            System.Threading.Tasks.Task newTask = new System.Threading.Tasks.Task(() => DoWork());
+            newTask.Start();
+            newTask.Wait();
+        }
+
+    }
+}
